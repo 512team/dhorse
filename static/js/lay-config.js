aa728be9bf594e2011f218f5ef7a1ebf1939129a
cgw <guowei.cao@weimob.com>
2023-03-26 21:33:34 +0800

    精简js文件

--- static/js/lay-config.js
+++ static/js/lay-config.js
@@ -19,11 +19,11 @@ layui.config({
     miniTheme: "layuimini/miniTheme", // layuimini 主题扩展
     miniTongji: "layuimini/miniTongji", // layuimini 统计扩展
     step: 'step-lay/step', // 分步表单扩展
-    treetable: 'treetable-lay/treetable', //table树形扩展
-    tableSelect: 'tableSelect/tableSelect', // table选择扩展
-    iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
+    //treetable: 'treetable-lay/treetable', //table树形扩展
+    //tableSelect: 'tableSelect/tableSelect', // table选择扩展
+    //iconPickerFa: 'iconPicker/iconPickerFa', // fa图标选择扩展
     echarts: 'echarts/echarts', // echarts图表扩展
     echartsTheme: 'echarts/echartsTheme', // echarts图表主题扩展
-    wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
-    layarea: 'layarea/layarea', //  省市县区三级联动下拉选择器
+    //wangEditor: 'wangEditor/wangEditor', // wangEditor富文本扩展
+    //layarea: 'layarea/layarea', //  省市县区三级联动下拉选择器
 });
