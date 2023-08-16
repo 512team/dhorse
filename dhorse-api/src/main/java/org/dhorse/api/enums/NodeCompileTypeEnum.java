d07fa16d59c5b9913fbf0ca179df0ff845cc66ba
高金富 <gaojinfu@bjklb.com>
2023-08-16 11:33:04 +0800

    针对vue和react项目添加pnpm编译方式

---
+++ dhorse-api/src/main/java/org/dhorse/api/enums/NodeCompileTypeEnum.java
@@ -0,0 +1,24 @@
+package org.dhorse.api.enums;
+
+public enum NodeCompileTypeEnum {
+
+    NPM(1, "npm"),
+    PNPM(2, "pnpm");
+
+    private Integer code;
+
+    private String value;
+
+    private NodeCompileTypeEnum(Integer code, String value) {
+        this.code = code;
+        this.value = value;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
